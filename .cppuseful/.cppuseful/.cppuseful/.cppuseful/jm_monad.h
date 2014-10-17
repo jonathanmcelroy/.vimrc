a8ef2dc41b1cddbe780be4862a99ff8f3a6a48bb
Jonathan McElroy <jonathan_mcelroy@baylor.edu>
2014-10-17 14:22:36 -0500

    Removed .cppuseful recursion

--- .cppuseful/.cppuseful/.cppuseful/.cppuseful/jm_monad.h
+++
@@ -1,9 +0,0 @@
-#ifndef JM_MONAD_H
-#define JM_MONAD_H
-
-template <class M, class F>
-auto operator >>= (M&& m, F&& f) {
-    
-}
-
-#endif // define JM_MONAD_H
--- .cppuseful/.cppuseful/jm_consts.h
+++
@@ -1,6 +0,0 @@
-#ifndef JM_CONSTS_H
-#define JM_CONSTS_H
-
-const int INF = 2000000000;
-
-#endif // define JM_CONSTS_H
