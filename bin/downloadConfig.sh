68fed1faa8efea74e02ff1656a6f09acb1e5b6ed
Jonathan McElroy <jonathanpmcelroy@gmail.com>
2017-03-25 14:55:14 -0500

    Updated configuration

--- bin/downloadConfig.sh
+++ bin/downloadConfig.sh
@@ -13,9 +13,8 @@ else
 fi
 
 echo "Bootstrapping vim plugin: 'plug'"
-if ! [ -d "$HOME/.vim/autoload" ] ;then
-    mkdir -p "$HOME/.vim/autoload"
-    curl -fLo "$HOME/.vim/autoload/plug.vim" https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if ! [ -d "$HOME/.local/share/nvim/site/autoload" ] ;then
+    curl -fLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 #if ! [ -d $HOME/.vim/bundle/vundle ]; then
@@ -29,5 +28,5 @@ cp .cppuseful/ "$HOME" -rf
 cp .cuseful/ "$HOME" -rf
 
 echo "Copying over binary files"
-mkdir -p $HOME/bin
-find ./bin -perm /a+x -type f -exec cp {} $HOME/bin/ \;
+mkdir -p $HOME/.local/bin
+find ./bin -perm /a+x -type f -exec cp {} $HOME/.local/bin/ \;
--- bin/uploadConfig.sh
+++ bin/uploadConfig.sh
@@ -1,31 +1,38 @@
 #!/usr/bin/env bash
 WORKTREE=$HOME/configuration/
 
+echo "Creating configuration directory"
 mkdir -p $WORKTREE
 
+echo "Cloning configuration git project if it doesn't exist"
 cd $WORKTREE
 if ! [ -d .git ]; then
     git clone https://github.com/jonathanmcelroy/configuration.git $WORKTREE
 fi
 
+echo "Copying over files"
 cp ~/.vimrc .
 cp ~/.zshrc .
 cp ~/.cppuseful/ . -r
 cp ~/.cuseful/ . -r
 
+echo "Copying over binary files"
 mkdir -p bin
-find $HOME/bin -perm /a+x -type f -exec cp {} ./bin/ \;
+find $HOME/.local/bin -perm /a+x -type f -exec cp {} ./bin/ \;
 
+echo "Delting files from git"
 git add .
 DELFILES=$(git ls-files --deleted -z)
 if [ ! -z "$DELFILES" ]; then
     git rm "$DELFILES"
 fi
 
+echo "Commiting git"
 if ! git diff --cached --quiet; then
     git commit -m 'Updated configuration'
 fi
 
+echo "Pusning git"
 if ! git diff --stat origin/master --quiet; then
     git push
 fi
